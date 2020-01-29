27cbbedd80e72d5ed049a72d5952d354c20d1a5e
Dan Willemsen <dwillemsen@google.com>
2020-01-29 10:38:13 -0800

    cepheus: use BUILD_COPY_HEADERS
    Bug: 130719878
    Test: turn copy headers into an error by default; still builds
    Change-Id: Idf228b2e8c4a4d93c3ad43fcd9995564d4feb67d
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,6 +14,7 @@ DEVICE_PATH := device/xiaomi/cepheus
 # Build
 BUILD_BROKEN_PREBUILT_ELF_FILES := true
 BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
 
 # Architecture
 TARGET_ARCH := arm64
