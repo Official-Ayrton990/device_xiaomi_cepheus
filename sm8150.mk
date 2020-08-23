15c6db39de20cacc8277be4f5e44ede090a73de2
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2020-08-23 06:31:34 +0000

    nfc: fix NFC issues on AOSiP
    Rework sm8150.mk configuration and restore stock NFC conf
    
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- sm8150.mk
+++ sm8150.mk
@@ -249,11 +249,9 @@ PRODUCT_PACKAGES += \
     com.android.nfc_extras \
     com.gsma.services.nfc \
     com.nxp.nfc.nq \
-    NQNfcNci \
-    Tag
-
-PRODUCT_PACKAGES += \
     nfc_nci.nqx.default.hw \
+    NfcNci \
+    Tag \
     vendor.nxp.hardware.nfc@1.2-service
 
 PRODUCT_COPY_FILES += \
