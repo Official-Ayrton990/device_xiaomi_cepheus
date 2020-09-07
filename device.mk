b32a08d8e453f379aed16d07a124ef9ae68aeb4d
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2020-09-07 12:27:49 -0500

    cepheus: device: don't fetch FOD permission from rom source
    Device tree now includes .xml permission for FingerprintInScreen, so remove the fetcher
    from device.mk so everyone can build succesfully
    
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- device.mk
+++ device.mk
@@ -153,7 +153,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
-    vendor/aosip/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 
 # FlickerFree
 PRODUCT_PACKAGES += \
