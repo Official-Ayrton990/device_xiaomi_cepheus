9da1906b2a3bb3b762e8364f375eefb0e7367da1
Lucchetto <lucchetto.tie@live.com>
2020-09-10 08:35:19 +0200

    zippo: build libavservices_minijail_vendor
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    
    Conflicts:
    	device.mk

--- device.mk
+++ device.mk
@@ -233,6 +233,10 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Media
+PRODUCT_PACKAGES += \
+    libavservices_minijail_vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
