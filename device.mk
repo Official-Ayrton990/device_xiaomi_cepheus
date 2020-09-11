47bfb6ab21ac88cb4dda88a538ccec23db1e2d4b
Jyotiraditya <imjyotiraditya@pm.me>
2020-09-11 18:19:05 +0000

    cepheus: Build signapk jar
    Test: build
    Signed-off-by: Jyotiraditya <imjyotiraditya@pm.me>
    Change-Id: Iab633fc9d450ec1354308a87f300550df73553bb
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- device.mk
+++ device.mk
@@ -210,7 +210,11 @@ PRODUCT_PACKAGES += \
     libhidltransport \
     libhidltransport.vendor
 
-# HotwordEnrollement
+# Signapk
+PRODUCT_HOST_PACKAGES += \
+    signapk
+
+# Hotword Enrollment
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml \
     $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
